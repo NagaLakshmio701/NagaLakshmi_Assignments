ad779bb357c97b030ce943ffbb27261908553de0
NagaLakshmio701 <nagalakshmisingamsetty9390@gmail.com>
2024-07-16 17:29:39 +0530

    commit

---
+++ Assignments/Assignment_8/1.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+
+namespace Assignment_8
+{
+    internal class Program
+    {
+
+        class demo1
+        {
+            private List<String> list;
+            public demo1(List<String> list)
+            {
+                this.list = list;
+            }
+            public void Write(string s)
+            {
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    foreach (string i in list)
+                    {
+                        sw.WriteLine(i);
+                    }
+                }
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter total number of names");
+                int n = int.Parse(Console.ReadLine());
+                List <string > list = new List<string>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"enter {i + 1} name");
+                    string n1= Console.ReadLine();
+                    list.Add(n1);
+                }
+
+                Console.WriteLine("enter file path to store the names");
+                String s = Console.ReadLine();
+                demo1 s1 = new demo1(list);
+                s1.Write(s);
+
+
+            }
+        }
+    }
+
+}
---
+++ Assignments/Assignment_8/2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+    class car
+    {
+        public string Model { get; set; }
+        public int year { get; set; }
+
+        public void Read(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Model = sr.ReadLine();
+                Console.WriteLine(Model);
+                year = int.Parse (sr.ReadLine());
+                Console.WriteLine(year);
+
+            }
+        }
+
+        public void Write(string path)
+        {
+            using (StreamWriter sr = new StreamWriter(path))
+            {
+                sr.WriteLine(Model);
+                sr.Write(year);
+            }
+        }
+    }
+
+    class demo2
+    {
+        static void Main(string[] args)
+        {
+
+            car ob = new car();
+            Console.WriteLine("enter car model");
+            ob.Model = Console.ReadLine();
+            Console.WriteLine("enter year ");
+            ob.year = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter path");
+            string path = Console.ReadLine();
+            ob.Write(path);
+            ob.Read(path);
+
+
+        }
+    }
+}
+
+
