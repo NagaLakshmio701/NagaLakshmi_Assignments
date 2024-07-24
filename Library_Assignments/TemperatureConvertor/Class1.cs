cad08e8d99f0df61dd8fdc25038d791b98b5d97d
NagaLakshmio701 <nagalakshmisingamsetty9390@gmail.com>
2024-07-24 15:07:16 +0530

    s

---
+++ Library_Assignments/TemperatureConvertor/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace TemperatureConvertor
+{
+    public class Temperature
+    {
+
+
+
+        public double CTF(double temp)
+        {
+            return (temp - 32) * 5 / 9;
+        }
+
+        public double FTC(double temp)
+        {
+            return (temp * 9 / 5) + 32;
+        }
+
+       
+    }
+}
